6cb522f30d4187e9e5b16e9583fe75f001e70d9a
dennolouis <dennolouis@gmail.com>
2022-03-10 10:16:04 -0500

    enemy movement:

--- Enemy.js
+++ Enemy.js
@@ -2,6 +2,9 @@ export default class Enemy{
     constructor(x, y, color, health){
         this.x = x;
         this.y = y;
+        this.speed = 0.5;
+        this.xDirection = 1;
+        this.yDirection = 1;
         this.color = color;
         this.health = health;
         this.width = 50;
@@ -9,6 +12,7 @@ export default class Enemy{
     }
 
     draw(ctx){
+        this.move();
         ctx.fillStyle = this.color;
         if(this.health > 1){
             ctx.strokeStyle = "white";
@@ -24,6 +28,19 @@ export default class Enemy{
         ctx.fillStyle = "black";
         ctx.font = "25px Arial";
         ctx.fillText(this.health, this.x + this.width/3.5, this.y + this.height/1.5);
+    }
+
+    move(){
+        
+        this.x += this.xDirection
+        this.y += this.yDirection
+
+       if(this.x < 0 | this.x > 500){
+           this.xDirection = -this.xDirection;
+       }
+       if(this.y < 0 | this.y > 250){
+            this.yDirection = -this.yDirection;
+        }
     }
 
     takeDamage(damage){
